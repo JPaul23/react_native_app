1e44fd1b0df4f9dbd8216bf5c54c5ea6669edfaf
JPaul23 <73589069+JPaul23@users.noreply.github.com>
2022-11-18 09:50:31 +0200

    finishing the subInfo

--- components/Buton.js
+++ components/Buton.js
@@ -26,10 +26,26 @@ export const CircleButon = ({ imgUrl, handlePress, ...props }) => {
         </TouchableOpacity>
     )
 }
-export const RectButton = () => {
+export const RectButton = ({ minWidth, fontSize, handlePress, ...props }) => {
     return (
-        <View>
-            <Text>Button</Text>
-        </View>
+        <TouchableOpacity
+            style={{
+                backgroundColor: COLORS.primary,
+                borderRadius: SIZES.extraLarge,
+                minWidth: minWidth,
+                padding: SIZES.small,
+                ...props,
+            }}
+            onPress={handlePress}
+        >
+            <Text style={{
+                fontFamily: FONTS.semiBold,
+                fontSize: fontSize,
+                color: COLORS.white,
+                textAlign: 'center'
+            }}>
+                Place a bid
+            </Text>
+        </TouchableOpacity>
     )
 }
