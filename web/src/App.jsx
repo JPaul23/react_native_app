59b8b4ae5ff9d4fe0c875c7b32bddab2f6184688
JPaul23 <73589069+JPaul23@users.noreply.github.com>
2022-12-01 22:15:48 +0200

    finishing up the web showcase

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,5 +1,6 @@
 import { Download, Features, SectionWrapper } from './components';
 import assets from './assets';
+import styles from './styles/Global';
 
 const App = () => {
   return (
@@ -8,10 +9,37 @@ const App = () => {
         title="Your Own Store of Nifty NFTS. Start Selling & Growing"
         description="Buy, Store, Collect NFTS, exchange & earn crypto. Join 25+ million people using ProNef MarketPlace."
 
-        ShowBtn
+        showBtn
         MockupImg={assets.homeHero}
         banner="banner"
       />
+      <SectionWrapper
+        title="Smart User Interface Marketplace"
+        description="Experience a buttery UI of ProNef NFT Marketplace. Smooth constant colors of a fluent UI design."
+        MockupImg={assets.homeCards}
+        reverse
+      />
+      <Features />
+
+      <SectionWrapper
+        title="Deployment"
+        description="ProNef is built using Expo which runs natively on all users' devices. You can easily get your app into people's hands."
+
+        MockupImg={assets.feature}
+        reverse
+      />
+      <SectionWrapper
+        title="The app contains two screens. The first screen lists all NFTs while the second one shows the details of a specific NFT."
+        MockupImg={assets.mockup}
+        banner='banner02'
+      />
+      <Download />
+
+      <div className='px-4 py-2 justify-center items-center bg-primary flex-col text-center banner04'>
+        <p className={`${styles.pText} ${styles.whiteText}`}>Made with love by {" "}
+        <span className='bold'>Paul</span>
+        </p>
+      </div>
     </>
   )
 }
--- web/src/components/SectionWrapper.jsx
+++ web/src/components/SectionWrapper.jsx
@@ -1,8 +1,45 @@
-import React from 'react'
+import styles from '../styles/Global';
+import assets from '../assets';
+import Button from './Button';
 
-const SectionWrapper = () => {
+const SectionWrapper = ({ title, description, showBtn, MockupImg, banner, reverse }) => {
   return (
-    <div>SectionWrapper</div>
+    <div className={`min-h-screen ${styles.section} ${reverse ? styles.bgWhite : styles.bgPrimary} ${banner}`}>
+
+      <div className={`flex items-center ${reverse ? styles.boxReverseClass : styles.boxClass}`}>
+        <div className={`${styles.descDiv}
+        ${reverse ? 'fadeRightMini' : 'fadeLeftMini'}
+        ${reverse ? styles.textRight : styles.textLeft}
+        `}>
+          <h1 className={`
+          ${reverse ? styles.blackText : styles.whiteText}
+          ${styles.h1Text}`
+          }>
+            {title}
+          </h1>
+          <p className={`
+        ${reverse ? styles.blackText : styles.whiteText}
+          ${styles.descriptionText}
+          `}>
+            {description}
+          </p>
+          {showBtn && (
+            <Button
+              assetUrl={assets.expo}
+              link='deployed url'
+            />
+          )}
+        </div>
+
+        <div className={`flex-1 ${styles.flexCenter} p-8 sm:px-0`}>
+
+          <img src={MockupImg} alt='mockup' className={`
+          ${reverse ? 'fadeLeftMini' : 'fadeRightMini'}
+          ${styles.sectionImg}`
+          } />
+        </div>
+      </div>
+    </div>
   )
 }
 
